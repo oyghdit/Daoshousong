7eef099f5d48399a09eaa66e8fc443455dcac797
weibo <2325882181@qq.com>
2019-12-05 18:14:31 +0800

    主页初步实现

--- app/src/main/java/com/example/home/fragment/HomeFragment.java
+++ app/src/main/java/com/example/home/fragment/HomeFragment.java
@@ -5,24 +5,23 @@ import android.view.Gravity;
 import android.view.View;
 import android.widget.TextView;
 import com.example.base.BaseFragment;
+import com.example.daoshousong.R;
+
+import static android.content.ContentValues.TAG;
 
 public class HomeFragment extends BaseFragment {    //主页面Fragment
 
-    private static final String TAG = HomeFragment.class.getSimpleName();
-    private TextView textView;
+
     @Override
     public View initView() {
         Log.e(TAG,"主页面的Fragment的UI被初始化了");
-        textView = new TextView(mContext);
-        textView.setGravity(Gravity.CENTER);
-        textView.setTextSize(25);
-        return textView;
+        View view = View.inflate(mContext,R.layout.fragment_home,null);
+        return view;
     }
 
     @Override
     public void initData() {
         super.initData();
         Log.e(TAG,"主页面的Fragment的数据被初始化了");
-        textView.setText("主页面内容");
     }
 }
